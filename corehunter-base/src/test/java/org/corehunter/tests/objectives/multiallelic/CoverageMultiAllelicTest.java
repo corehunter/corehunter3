71179de8cdd1edd245f950358b1462b8a6136b95
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 11:43:45 +0100

    formatted classes in org.corehunter.tests.objectives.multiallelic

--- corehunter-base/src/test/java/org/corehunter/tests/objectives/multiallelic/CoverageMultiAllelicTest.java
+++ corehunter-base/src/test/java/org/corehunter/tests/objectives/multiallelic/CoverageMultiAllelicTest.java
@@ -1,18 +1,22 @@
-/*******************************************************************************
- * Copyright 2015 Guy Davenport
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
 package org.corehunter.tests.objectives.multiallelic;
 
 import static org.corehunter.tests.TestData.ALLELES;
@@ -31,25 +35,25 @@ import org.junit.Test;
 
 /**
  * @author Guy Davenport
- *
  */
-public class CoverageMultiAllelicTest extends EvaluationTest
-{
-	/**
-	 * Test method for {@link org.corehunter.objectives.distance.multiallelic.CavalliSforzaEdwardsDistanceMultiAllelic#getDistance(int, int)}.
-	 */
-	@Test
-	public void test()
-	{
-		SimpleMultiAllelicGenotypeVariantData data = 
-				new SimpleMultiAllelicGenotypeVariantData(NAME, NAMES, MARKER_NAMES, ALLELE_NAMES, ALLELES) ;
+public class CoverageMultiAllelicTest extends EvaluationTest {
 
-		CoverageMultiAllelic distanceMetric = 
-				new CoverageMultiAllelic() ;
+    /**
+     * Test method for
+     * {@link org.corehunter.objectives.distance.multiallelic
+     * .CavalliSforzaEdwardsDistanceMultiAllelic#getDistance(int, int)}.
+     */
+    @Test
+    public void test() {
+        SimpleMultiAllelicGenotypeVariantData data
+                = new SimpleMultiAllelicGenotypeVariantData(NAME, NAMES, MARKER_NAMES, ALLELE_NAMES, ALLELES);
 
-		assertEquals("Evaluation for subset 1 is not correct!", COVERAGE_SUBSET1, 
-				distanceMetric.evaluate(new SubsetSolution(data.getIDs(), SUBSET1), data), PRECISION) ;
-		
-	}
+        CoverageMultiAllelic distanceMetric
+                = new CoverageMultiAllelic();
+
+        assertEquals("Evaluation for subset 1 is not correct!", COVERAGE_SUBSET1,
+                distanceMetric.evaluate(new SubsetSolution(data.getIDs(), SUBSET1), data), PRECISION);
+
+    }
 
 }
