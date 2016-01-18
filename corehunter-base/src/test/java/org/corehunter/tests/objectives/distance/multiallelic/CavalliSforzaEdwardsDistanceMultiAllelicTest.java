255e4148156f6f684f76d07ddec444643024f742
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 11:41:34 +0100

    formatted classes in org.corehunter.tests.objectives.distance.multiallelic

--- corehunter-base/src/test/java/org/corehunter/tests/objectives/distance/multiallelic/CavalliSforzaEdwardsDistanceMultiAllelicTest.java
+++ corehunter-base/src/test/java/org/corehunter/tests/objectives/distance/multiallelic/CavalliSforzaEdwardsDistanceMultiAllelicTest.java
@@ -1,18 +1,22 @@
-/*******************************************************************************
- * Copyright 2015 Guy Davenport
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
 package org.corehunter.tests.objectives.distance.multiallelic;
 
 import static org.corehunter.tests.TestData.ALLELES;
@@ -33,54 +37,54 @@ import org.junit.Test;
 
 /**
  * @author Guy Davenport
- *
  */
-public class CavalliSforzaEdwardsDistanceMultiAllelicTest
-{
+public class CavalliSforzaEdwardsDistanceMultiAllelicTest {
 
-	/**
-	 * Test method for {@link org.corehunter.objectives.distance.multiallelic.CavalliSforzaEdwardsDistanceMultiAllelic#getDistance(int, int)}.
-	 */
-	@Test
-	public void test()
-	{
-		SimpleMultiAllelicGenotypeVariantData data = new SimpleMultiAllelicGenotypeVariantData(NAME, NAMES, MARKER_NAMES, ALLELE_NAMES, ALLELES) ;
+    /**
+     * Test method for
+     * {@link org.corehunter.objectives.distance.multiallelic
+     * .CavalliSforzaEdwardsDistanceMultiAllelic#getDistance(int, int)}.
+     */
+    @Test
+    public void test() {
+        SimpleMultiAllelicGenotypeVariantData data = new SimpleMultiAllelicGenotypeVariantData(
+                NAME, NAMES, MARKER_NAMES, ALLELE_NAMES, ALLELES
+        );
 
-		CavalliSforzaEdwardsDistanceMultiAllelic distanceMetric = 
-				new CavalliSforzaEdwardsDistanceMultiAllelic(data) ;
-		
-		assertEquals("Data is not correct!", data, distanceMetric.getData()) ;
-		
-		assertEquals("Ids not correct!", SET, distanceMetric.getIDs()) ;
+        CavalliSforzaEdwardsDistanceMultiAllelic distanceMetric
+                = new CavalliSforzaEdwardsDistanceMultiAllelic(data);
 
-		Iterator<Integer> iteratorX = distanceMetric.getIDs().iterator() ;
-		Iterator<Integer> iteratorY ;
-		
-		Integer idX ;
-		Integer idY ;
-		
-		int i = 0 ;
-		int j = 0 ;
-		
-		while (iteratorX.hasNext())
-		{
-			idX = iteratorX.next() ;
-			
-			iteratorY = distanceMetric.getIDs().iterator() ;
-			
-			j = 0 ;
-			
-			while (iteratorY.hasNext())
-			{
-				idY = iteratorY.next() ;
-				
-				assertEquals("Distance["+i+"]["+j+"] not correct!", CAVALLI_SFORZA_EDWARDS_DISTANCES[i][j], distanceMetric.getDistance(idX, idY), PRECISION) ;
-				
-				++j ;
-			}
-			
-			++i ;
-		}
-	}
+        assertEquals("Data is not correct!", data, distanceMetric.getData());
+
+        assertEquals("Ids not correct!", SET, distanceMetric.getIDs());
+
+        Iterator<Integer> iteratorX = distanceMetric.getIDs().iterator();
+        Iterator<Integer> iteratorY;
+
+        Integer idX;
+        Integer idY;
+
+        int i = 0;
+        int j = 0;
+
+        while (iteratorX.hasNext()) {
+            idX = iteratorX.next();
+
+            iteratorY = distanceMetric.getIDs().iterator();
+
+            j = 0;
+
+            while (iteratorY.hasNext()) {
+                idY = iteratorY.next();
+
+                assertEquals("Distance[" + i + "][" + j + "] not correct!",
+                             CAVALLI_SFORZA_EDWARDS_DISTANCES[i][j], distanceMetric.getDistance(idX, idY), PRECISION);
+
+                ++j;
+            }
+
+            ++i;
+        }
+    }
 
 }
