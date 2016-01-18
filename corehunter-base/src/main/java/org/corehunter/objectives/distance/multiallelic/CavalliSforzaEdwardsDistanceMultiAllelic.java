979dbe963ce5f6be14d3bc0d8728ab0f8041e8f2
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 10:21:57 +0100

    formatted classes in package org.corehunter.objectives.distance.multiallelic

--- corehunter-base/src/main/java/org/corehunter/objectives/distance/multiallelic/CavalliSforzaEdwardsDistanceMultiAllelic.java
+++ corehunter-base/src/main/java/org/corehunter/objectives/distance/multiallelic/CavalliSforzaEdwardsDistanceMultiAllelic.java
@@ -1,13 +1,22 @@
-/*******************************************************************************
- * Copyright 2014 Herman De Beukelaer, Guy Davenport Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
 package org.corehunter.objectives.distance.multiallelic;
 
 import org.corehunter.data.MultiAllelicGenotypeVariantData;
@@ -17,58 +26,50 @@ import org.corehunter.objectives.distance.AbstractGenotypeVariantDistanceMetric;
  * @author Guy Davenport
  */
 public class CavalliSforzaEdwardsDistanceMultiAllelic extends
-    AbstractGenotypeVariantDistanceMetric<MultiAllelicGenotypeVariantData>
-{
+        AbstractGenotypeVariantDistanceMetric<MultiAllelicGenotypeVariantData> {
 
-	/**
-	 * @param data
-	 */
-	public CavalliSforzaEdwardsDistanceMultiAllelic(
-	    MultiAllelicGenotypeVariantData data)
-	{
-		super(data);
-	}
+    public CavalliSforzaEdwardsDistanceMultiAllelic(
+            MultiAllelicGenotypeVariantData data) {
+        super(data);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.corehunter.DistanceMatrixData#getDistance(int, int)
-	 */
-	@Override
-	public double getDistance(int idX, int idY)
-	{
-		double distance = 0;
+    /*
+     * (non-Javadoc)
+     * @see org.corehunter.DistanceMatrixData#getDistance(int, int)
+     */
+    @Override
+    public double getDistance(int idX, int idY) {
+        
+        double distance;
 
-		int numberOfMarkers = getData().getNumberOfMarkers();
-		int numberOfAlleles;
+        int numberOfMarkers = getData().getNumberOfMarkers();
+        int numberOfAlleles;
 
-		double markerSqDiff = 0;
-		double sumMarkerSqDiff = 0;
-		double sqrtDiff = 0;
-		double Pxla;
-		double Pyla;
+        double markerSqDiff = 0;
+        double sumMarkerSqDiff = 0;
+        double sqrtDiff;
+        double pxla;
+        double pyla;
 
-		for (int markerIndex = 0; markerIndex < numberOfMarkers; ++markerIndex)
-		{
-			numberOfAlleles = getData().getNumberOfAlleles(markerIndex);
+        for (int markerIndex = 0; markerIndex < numberOfMarkers; ++markerIndex) {
+            numberOfAlleles = getData().getNumberOfAlleles(markerIndex);
 
-			for (int alleleIndex = 0; alleleIndex < numberOfAlleles; ++alleleIndex)
-			{
-				Pxla = getData().getAlelleFrequency(idX, markerIndex, alleleIndex);
-				Pyla = getData().getAlelleFrequency(idY, markerIndex, alleleIndex);
+            for (int alleleIndex = 0; alleleIndex < numberOfAlleles; ++alleleIndex) {
+                pxla = getData().getAlelleFrequency(idX, markerIndex, alleleIndex);
+                pyla = getData().getAlelleFrequency(idY, markerIndex, alleleIndex);
 
-				if (Pxla >= 0 && Pyla >= 0)
-				{
-					sqrtDiff = Math.sqrt(Pxla) - Math.sqrt(Pyla);
-					markerSqDiff += (sqrtDiff) * (sqrtDiff);
-				}
-			}
+                if (pxla >= 0 && pyla >= 0) {
+                    sqrtDiff = Math.sqrt(pxla) - Math.sqrt(pyla);
+                    markerSqDiff += (sqrtDiff) * (sqrtDiff);
+                }
+            }
 
-			sumMarkerSqDiff += markerSqDiff;
-		}
+            sumMarkerSqDiff += markerSqDiff;
+        }
 
-		distance = 1.0 / (Math.sqrt(2.0 * numberOfMarkers))
-		    * Math.sqrt(sumMarkerSqDiff);
+        distance = 1.0 / (Math.sqrt(2.0 * numberOfMarkers))
+                * Math.sqrt(sumMarkerSqDiff);
 
-		return distance;
-	}
+        return distance;
+    }
 }
--- corehunter-base/src/main/java/org/corehunter/objectives/distance/multiallelic/ModifiedRogersDistanceMultiAllelic.java
+++ corehunter-base/src/main/java/org/corehunter/objectives/distance/multiallelic/ModifiedRogersDistanceMultiAllelic.java
@@ -1,13 +1,22 @@
-/*******************************************************************************
- * Copyright 2014 Herman De Beukelaer, Guy Davenport Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
 package org.corehunter.objectives.distance.multiallelic;
 
 import org.corehunter.data.MultiAllelicGenotypeVariantData;
@@ -17,55 +26,45 @@ import org.corehunter.objectives.distance.AbstractGenotypeVariantDistanceMetric;
  * @author Guy Davenport
  */
 public class ModifiedRogersDistanceMultiAllelic extends
-    AbstractGenotypeVariantDistanceMetric<MultiAllelicGenotypeVariantData>
-{
+        AbstractGenotypeVariantDistanceMetric<MultiAllelicGenotypeVariantData> {
 
-	/**
-	 * @param data
-	 */
-	public ModifiedRogersDistanceMultiAllelic(MultiAllelicGenotypeVariantData data)
-	{
-		super(data);
-	}
+    public ModifiedRogersDistanceMultiAllelic(MultiAllelicGenotypeVariantData data) {
+        super(data);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.corehunter.DistanceMatrixData#getDistance(int, int)
-	 */
-	@Override
-	public double getDistance(int idX, int idY)
-	{
-		double distance = 0;
+    /*
+     * (non-Javadoc)
+     * @see org.corehunter.DistanceMatrixData#getDistance(int, int)
+     */
+    @Override
+    public double getDistance(int idX, int idY) {
+        
+        double distance;
 
-		int numberOfMarkers = getData().getNumberOfMarkers();
-		int numberOfAlleles;
+        int numberOfMarkers = getData().getNumberOfMarkers();
+        int numberOfAlleles;
 
-		double markerSqDiff = 0;
-		double sumMarkerSqDiff = 0;
+        double markerSqDiff = 0;
+        double sumMarkerSqDiff = 0;
 
-		for (int markerIndex = 0; markerIndex < numberOfMarkers; ++markerIndex)
-		{
-			numberOfAlleles = getData().getNumberOfAlleles(markerIndex);
+        for (int markerIndex = 0; markerIndex < numberOfMarkers; ++markerIndex) {
+            numberOfAlleles = getData().getNumberOfAlleles(markerIndex);
 
-			for (int alleleIndex = 0; alleleIndex < numberOfAlleles; ++alleleIndex)
-			{
-				double Pxla = getData().getAlelleFrequency(idX, markerIndex,
-				    alleleIndex);
-				double Pyla = getData().getAlelleFrequency(idY, markerIndex,
-				    alleleIndex);
+            for (int alleleIndex = 0; alleleIndex < numberOfAlleles; ++alleleIndex) {
+                double pxla = getData().getAlelleFrequency(idX, markerIndex, alleleIndex);
+                double pyla = getData().getAlelleFrequency(idY, markerIndex, alleleIndex);
 
-				if (Pxla >= 0 && Pyla >= 0)
-				{
-					markerSqDiff += (Pxla - Pyla) * (Pxla - Pyla);
-				}
-			}
+                if (pxla >= 0 && pyla >= 0) {
+                    markerSqDiff += (pxla - pyla) * (pxla - pyla);
+                }
+            }
 
-			sumMarkerSqDiff += markerSqDiff;
-		}
+            sumMarkerSqDiff += markerSqDiff;
+        }
 
-		distance = 1.0 / (Math.sqrt(2.0 * numberOfMarkers))
-		    * Math.sqrt(sumMarkerSqDiff);
+        distance = 1.0 / (Math.sqrt(2.0 * numberOfMarkers))
+                * Math.sqrt(sumMarkerSqDiff);
 
-		return distance;
-	}
+        return distance;
+    }
 }
