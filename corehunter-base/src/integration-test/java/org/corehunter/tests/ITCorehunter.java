cc4978d20893f8b59627b5c7fc3302df2a501641
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 14:07:45 +0100

    Travis test: this should pass checkstyle

--- corehunter-base/src/integration-test/java/org/corehunter/tests/ITCorehunter.java
+++ corehunter-base/src/integration-test/java/org/corehunter/tests/ITCorehunter.java
@@ -16,6 +16,7 @@
 /* specific language governing permissions and limitations      */
 /* under the License.                                           */
 /*--------------------------------------------------------------*/
+
 package org.corehunter.tests;
 
 import static org.junit.Assert.assertEquals;
