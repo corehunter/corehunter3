14e81e4e5169bd224a97baf04285fcf730d41176
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 10:18:59 +0100

    formatted classes in package org.corehunter.objectives.distance

--- corehunter-base/src/main/java/org/corehunter/objectives/distance/AbstractGenotypeVariantDistanceMetric.java
+++ corehunter-base/src/main/java/org/corehunter/objectives/distance/AbstractGenotypeVariantDistanceMetric.java
@@ -1,13 +1,22 @@
-/*******************************************************************************
- * Copyright 2014 Herman De Beukelaer, Guy Davenport Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
 package org.corehunter.objectives.distance;
 
 import java.util.Set;
@@ -18,39 +27,35 @@ import org.corehunter.data.GenotypeVariantData;
  * @author Guy Davenport
  */
 public abstract class AbstractGenotypeVariantDistanceMetric<GenotypeVariantDataType extends GenotypeVariantData>
-    implements GenotypeVariantDistanceMetric<GenotypeVariantDataType>
-{
-	private GenotypeVariantDataType	data;
+        implements GenotypeVariantDistanceMetric<GenotypeVariantDataType> {
 
-	public AbstractGenotypeVariantDistanceMetric(GenotypeVariantDataType data)
-	{
-		super();
+    private GenotypeVariantDataType data;
 
-		setData(data);
-	}
+    public AbstractGenotypeVariantDistanceMetric(GenotypeVariantDataType data) {
+        super();
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.jamesframework.core.problems.datatypes.SubsetData#getIDs()
-	 */
-	@Override
-	public final Set<Integer> getIDs()
-	{
-		return data.getIDs();
-	}
+        setData(data);
+    }
 
-	 /*
-   * (non-Javadoc)
-   * @see org.corehunter.objectives.distance.GenotypeVariantDistanceMetric.SubsetData#getData()
-   */
-  @Override
-	public final GenotypeVariantDataType getData()
-	{
-		return data;
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.jamesframework.core.problems.datatypes.SubsetData#getIDs()
+     */
+    @Override
+    public final Set<Integer> getIDs() {
+        return data.getIDs();
+    }
 
-	protected final void setData(GenotypeVariantDataType data)
-	{
-		this.data = data;
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.corehunter.objectives.distance.GenotypeVariantDistanceMetric.SubsetData#getData()
+     */
+    @Override
+    public final GenotypeVariantDataType getData() {
+        return data;
+    }
+
+    protected final void setData(GenotypeVariantDataType data) {
+        this.data = data;
+    }
 }
