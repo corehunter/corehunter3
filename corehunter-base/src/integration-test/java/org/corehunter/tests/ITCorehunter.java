8e79a98951610b9461379cea876da1d8881140ed
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 11:55:18 +0100

    formatted integration test in org.corehunter.tests

--- corehunter-base/src/integration-test/java/org/corehunter/tests/ITCorehunter.java
+++ corehunter-base/src/integration-test/java/org/corehunter/tests/ITCorehunter.java
@@ -1,13 +1,22 @@
-/*******************************************************************************
- * Copyright 2014 Guy Davenport Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+ /* Licensed to the Apache Software Foundation (ASF) under one   */
+ /* or more contributor license agreements.  See the NOTICE file */
+ /* distributed with this work for additional information        */
+ /* regarding copyright ownership.  The ASF licenses this file   */
+ /* to you under the Apache License, Version 2.0 (the            */
+ /* "License"); you may not use this file except in compliance   */
+ /* with the License.  You may obtain a copy of the License at   */
+ /*                                                              */
+ /*   http://www.apache.org/licenses/LICENSE-2.0                 */
+ /*                                                              */
+ /* Unless required by applicable law or agreed to in writing,   */
+ /* software distributed under the License is distributed on an  */
+ /* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+ /* KIND, either express or implied.  See the License for the    */
+ /* specific language governing permissions and limitations      */
+ /* under the License.                                           */
+ /*--------------------------------------------------------------*/
+
 package org.corehunter.tests;
 
 import static org.junit.Assert.assertEquals;
@@ -20,65 +29,60 @@ import org.junit.Test;
 /**
  * @author Guy Davenport
  */
-public class ITCorehunter extends TestData
-{
-	/**
-	 * Test method for
-	 * {@link org.corehunter.Corehunter#executeRandomDescent(org.corehunter.DistanceMatrixData, org.jamesframework.core.problems.objectives.Objective, int)}
-	 * .
-	 */
-        //@Test
-	public void testExecuteDistanceMatrix()
-	{
-		CorehunterArguments arguments = new CorehunterArguments(2) ;
-		
-		arguments.setDataset(DATA);
-		
-		Corehunter corehunter = new Corehunter(arguments);
+public class ITCorehunter extends TestData {
 
-		corehunter.setTimeLimit(2);
+    /**
+     * Test method for
+     * {@link org.corehunter.Corehunter#executeRandomDescent
+     * (org.corehunter.DistanceMatrixData, org.jamesframework.core.problems.objectives.Objective, int)}
+     * .
+     */
+    //@Test
+    public void testExecuteDistanceMatrix() {
+        CorehunterArguments arguments = new CorehunterArguments(2);
 
-		SubsetSolution result = corehunter.execute();
+        arguments.setDataset(DATA);
 
-		assertEquals(SUBSET1, result.getSelectedIDs());
-	}
+        Corehunter corehunter = new Corehunter(arguments);
 
-	/**
-	 * Test method for {@link org.corehunter.Corehunter#getTimeLimit()}.
-	 */
-	@Test
-	public void testGetTimeLimit()
-	{
+        corehunter.setTimeLimit(2);
 
-        }
+        SubsetSolution result = corehunter.execute();
 
-	/**
-	 * Test method for {@link org.corehunter.Corehunter#setTimeLimit(long)}.
-	 */
-	@Test
-	public void testSetTimeLimit()
-	{
+        assertEquals(SUBSET1, result.getSelectedIDs());
+    }
 
-	}
+    /**
+     * Test method for {@link org.corehunter.Corehunter#getTimeLimit()}.
+     */
+    @Test
+    public void testGetTimeLimit() {
 
-	/**
-	 * Test method for {@link org.corehunter.Corehunter#getSearchListener()}.
-	 */
-	@Test
-	public void testGetSearchListener()
-	{
+    }
 
-	}
+    /**
+     * Test method for {@link org.corehunter.Corehunter#setTimeLimit(long)}.
+     */
+    @Test
+    public void testSetTimeLimit() {
 
-	/**
-	 * Test method for
-	 * {@link org.corehunter.Corehunter#setSearchListener(org.jamesframework.core.search.listeners.SearchListener)}
-	 * .
-	 */
-	@Test
-	public void testSetSearchListener()
-	{
+    }
 
-	}
+    /**
+     * Test method for {@link org.corehunter.Corehunter#getSearchListener()}.
+     */
+    @Test
+    public void testGetSearchListener() {
+
+    }
+
+    /**
+     * Test method for
+     * {@link org.corehunter.Corehunter#setSearchListener(org.jamesframework.core.search.listeners.SearchListener)} .
+     */
+    @Test
+    public void testSetSearchListener() {
+
+    }
 
 }
