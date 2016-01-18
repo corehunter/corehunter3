c152f4477c89c7764809a591a3e09a187e0e6064
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-01-18 10:39:54 +0100

    formatted classes in org.corehunter.tests

--- corehunter-base/src/test/java/org/corehunter/tests/MockFeature.java
+++ corehunter-base/src/test/java/org/corehunter/tests/MockFeature.java
@@ -1,20 +1,23 @@
-package org.corehunter.tests;
-/*******************************************************************************
- * Copyright 2014 Guy Davenport
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
 
+package org.corehunter.tests;
 
 import uno.informatics.common.model.DataType;
 import uno.informatics.common.model.Feature;
@@ -28,206 +31,161 @@ import uno.informatics.common.model.ScaleType;
  * @author Guy Davenport
  *
  */
-public class MockFeature implements Feature
-{
-	private Scale scale;
+public class MockFeature implements Feature {
 
-	public MockFeature()
-  {
-	  super();
-	  
-  }
+    private Scale scale;
 
+    public MockFeature() {
+        super();
 
-	/**
-	 * @param dataType
-	 * @param scaleType
-	 */
-  public MockFeature(DataType dataType, ScaleType scaleType)
-  {
-  	scale = new MockScale(dataType, scaleType) ;
-  }
-  
-	/**
-	 * @param dataType
-	 * @param scaleType
-	 * @param min
-	 * @param max
-	 */
-  public MockFeature(DataType dataType, ScaleType scaleType, double min, double max)
-  {
-  	scale = new MockRangeScale(dataType, scaleType, min, max) ;
-  }
-  
-	/**
-	 * @param dataType
-	 * @param scaleType
-	 * @param min
-	 * @param max
-	 */
-  public MockFeature(DataType dataType, ScaleType scaleType, int min, int max)
-  {
-  	scale = new MockRangeScale(dataType, scaleType, min, max) ;
-  }
-  
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Identifier#getAbbreviation()
-	 */
-	@Override
-	public String getAbbreviation()
-	{
-		
-		return null;
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Identifier#setAbbreviation(java.lang.String)
-	 */
-	@Override
-	public void setAbbreviation(String value)
-	{
-		
+    public MockFeature(DataType dataType, ScaleType scaleType) {
+        scale = new MockScale(dataType, scaleType);
+    }
 
-	}
+    public MockFeature(DataType dataType, ScaleType scaleType, double min, double max) {
+        scale = new MockRangeScale(dataType, scaleType, min, max);
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Identifier#getType()
-	 */
-	@Override
-	public OntologyTerm getType()
-	{
-		
-		return null;
-	}
+    public MockFeature(DataType dataType, ScaleType scaleType, int min, int max) {
+        scale = new MockRangeScale(dataType, scaleType, min, max);
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Identifier#setType(uno.informatics.model.OntologyTerm)
-	 */
-	@Override
-	public void setType(OntologyTerm value)
-	{
-		
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Identifier#getAbbreviation()
+     */
+    @Override
+    public String getAbbreviation() {
 
-	}
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Namable#getName()
-	 */
-	@Override
-	public String getName()
-	{
-		
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Identifier#setAbbreviation(java.lang.String)
+     */
+    @Override
+    public void setAbbreviation(String value) {
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Namable#setName(java.lang.String)
-	 */
-	@Override
-	public void setName(String name)
-	{
-		
+    }
 
-	}
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Identifier#getType()
+     */
+    @Override
+    public OntologyTerm getType() {
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Identifiable#getUniqueIdentifier()
-	 */
-	@Override
-	public String getUniqueIdentifier()
-	{
-		
-		return null;
-	}
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Identifiable#setUniqueIdentifier(java.lang.String)
-	 */
-	@Override
-	public void setUniqueIdentifier(String uniqueIdentifier)
-	{
-		
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Identifier#setType(uno.informatics.model.OntologyTerm)
+     */
+    @Override
+    public void setType(OntologyTerm value) {
 
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Describable#getDescription()
-	 */
-	@Override
-	public String getDescription()
-	{
-		
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Namable#getName()
+     */
+    @Override
+    public String getName() {
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Describable#setDescription(java.lang.String)
-	 */
-	@Override
-	public void setDescription(String description)
-	{
-		
+        return null;
+    }
 
-	}
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Namable#setName(java.lang.String)
+     */
+    @Override
+    public void setName(String name) {
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Feature#getProperty()
-	 */
-	@Override
-	public Property getProperty()
-	{
-		
-		return null;
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Feature#setProperty(uno.informatics.model.Property)
-	 */
-	@Override
-	public void setProperty(Property value)
-	{
-		
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Identifiable#getUniqueIdentifier()
+     */
+    @Override
+    public String getUniqueIdentifier() {
 
-	}
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Feature#getScale()
-	 */
-	@Override
-	public Scale getScale()
-	{
-		
-		return scale ;
-	}
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Identifiable#setUniqueIdentifier(java.lang.String)
+     */
+    @Override
+    public void setUniqueIdentifier(String uniqueIdentifier) {
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Feature#setScale(uno.informatics.model.Scale)
-	 */
-	@Override
-	public void setScale(Scale value)
-	{
-		
+    }
 
-	}
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Describable#getDescription()
+     */
+    @Override
+    public String getDescription() {
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Feature#getMethod()
-	 */
-	@Override
-	public Method getMethod()
-	{
-		
-		return null;
-	}
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see uno.informatics.model.Feature#setMethod(uno.informatics.model.Method)
-	 */
-	@Override
-	public void setMethod(Method value)
-	{
-		
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Describable#setDescription(java.lang.String)
+     */
+    @Override
+    public void setDescription(String description) {
 
-	}
+    }
+
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Feature#getProperty()
+     */
+    @Override
+    public Property getProperty() {
+
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Feature#setProperty(uno.informatics.model.Property)
+     */
+    @Override
+    public void setProperty(Property value) {
+
+    }
+
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Feature#getScale()
+     */
+    @Override
+    public Scale getScale() {
+
+        return scale;
+    }
+
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Feature#setScale(uno.informatics.model.Scale)
+     */
+    @Override
+    public void setScale(Scale value) {
+
+    }
+
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Feature#getMethod()
+     */
+    @Override
+    public Method getMethod() {
+
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see uno.informatics.model.Feature#setMethod(uno.informatics.model.Method)
+     */
+    @Override
+    public void setMethod(Method value) {
+
+    }
 
 }
