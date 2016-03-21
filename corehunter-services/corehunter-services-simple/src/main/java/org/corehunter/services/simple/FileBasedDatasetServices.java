c8815c3e9557f7b7594d07883800aa0e29c62ee7
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-03-21 13:18:23 +0100

    unify license headers

--- corehunter-services/corehunter-services-simple/src/main/java/org/corehunter/services/simple/FileBasedDatasetServices.java
+++ corehunter-services/corehunter-services-simple/src/main/java/org/corehunter/services/simple/FileBasedDatasetServices.java
@@ -1,18 +1,21 @@
-/*******************************************************************************
- * Copyright 2016 Guy Davenport
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
 
 package org.corehunter.services.simple;
 
@@ -26,9 +29,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
-import java.util.UUID;
 
 import org.corehunter.data.NamedData;
 import org.corehunter.data.simple.SimpleBiAllelicGenotypeVariantData;
@@ -41,14 +42,8 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 import uno.informatics.common.io.FileType;
-import uno.informatics.data.Data;
 import uno.informatics.data.Dataset;
-import uno.informatics.data.SimpleEntity;
 import uno.informatics.data.dataset.DatasetException;
-import uno.informatics.data.feature.array.ArrayFeatureDataset;
-import uno.informatics.data.feature.array.ZipFeatureDataReader;
-import uno.informatics.data.feature.array.ZipFeatureDatasetWriter;
-import uno.informatics.data.pojo.SimpleEntityPojo;
 
 public class FileBasedDatasetServices implements DatasetServices {
     private static final String DATASETS = "datasets.xml";
--- corehunter-services/corehunter-services-simple/src/test/java/org/corehunter/services/simple/test/FileBasedDatasetServicesTest.java
+++ corehunter-services/corehunter-services-simple/src/test/java/org/corehunter/services/simple/test/FileBasedDatasetServicesTest.java
@@ -1,41 +1,37 @@
-/*******************************************************************************
- * Copyright 2016 Guy Davenport
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
 
 package org.corehunter.services.simple.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
-import org.corehunter.data.NamedData;
-import org.corehunter.data.simple.SimplePhenotypicTraitData;
-import org.corehunter.services.DataType;
 import org.corehunter.services.simple.FileBasedDatasetServices;
 import org.junit.Test;
 
-import uno.informatics.common.io.FileType;
 import uno.informatics.data.Dataset;
-import uno.informatics.data.SimpleEntity;
 import uno.informatics.data.pojo.DatasetPojo;
 
 public class FileBasedDatasetServicesTest {
