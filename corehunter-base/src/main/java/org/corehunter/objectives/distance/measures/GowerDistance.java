d65ec9f1142554ed2283ec2a584e5dba59923e51
Herman De Beukelaer <herman.debeukelaer@ugent.be>
2016-04-07 13:36:47 +0200

    Gower: cache scale types and ranges #32

--- corehunter-base/src/main/java/org/corehunter/objectives/distance/measures/GowerDistance.java
+++ corehunter-base/src/main/java/org/corehunter/objectives/distance/measures/GowerDistance.java
@@ -19,12 +19,16 @@
 
 package org.corehunter.objectives.distance.measures;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import org.corehunter.data.CoreHunterData;
 import org.corehunter.exceptions.CoreHunterException;
 import uno.informatics.data.Feature;
 import uno.informatics.data.Scale;
 import uno.informatics.data.dataset.FeatureData;
+import uno.informatics.data.dataset.FeatureDataRow;
 
 /**
  * @author Herman De Beukelaer, Guy Davenport
@@ -35,17 +39,46 @@ public class GowerDistance extends CachedDistanceMeasure {
     private static final int DISCRETE_SCALE_TYPE = 1;
     private static final int RANGED_SCALE_TYPE = 2;
 
+    // scale type and range cache
+    private final Map<FeatureData, FeatureMetadata> cache;
+    
+    private class FeatureMetadata {
+        
+        private final int numFeatures;
+        private final int[] scaleTypes;
+        private final double[] ranges;
+
+        public FeatureMetadata(int numFeatures, int[] scaleTypes, double[] ranges) {
+            this.numFeatures = numFeatures;
+            this.scaleTypes = scaleTypes;
+            this.ranges = ranges;
+        }
+
+        public int getNumFeatures() {
+            return numFeatures;
+        }
+
+        public int[] getScaleTypes() {
+            return scaleTypes;
+        }
+
+        public double[] getRanges() {
+            return ranges;
+        }
+        
+    }
+    
     public GowerDistance() {
-        super();
+        this(MissingValuesPolicy.FLOOR);
     }
 
     public GowerDistance(MissingValuesPolicy policy) {
         super(policy);
+        cache = new HashMap<>();
     }
     
-    // TODO cache scale types and ranges
     @Override
-    public double computeDistance(int idX, int idY, CoreHunterData data, MissingValuesPolicy missingDataPolicy) {
+    public double computeDistance(int idX, int idY, CoreHunterData data, MissingValuesPolicy missingValuesPolicy) {
 
         if(idX == idY){
             return 0.0;
@@ -57,57 +90,23 @@ public class GowerDistance extends CachedDistanceMeasure {
             throw new CoreHunterException("Phenotypes are required for Gower distance.");
         }
         
-        Object[][] values = phenotypes.getValuesAsArray();
-        Feature[] features = phenotypes.getFeaturesAsArray();
+        // get feature data rows of both items
+        FeatureDataRow rowX = phenotypes.getRow(idX);
+        FeatureDataRow rowY = phenotypes.getRow(idY);
+        
+        // get cached scale types and ranges
+        FeatureMetadata featureMetadata = getFeatureMetadata(phenotypes);
+        int[] scaleTypes = featureMetadata.getScaleTypes();
+        double[] ranges  = featureMetadata.getRanges();
         
         double distSum = 0.0;
         double weightSum = 0.0;
-        for (int k = 0; k < features.length; k++) {
+        for (int k = 0; k < featureMetadata.getNumFeatures(); k++) {
             
-            Scale scale = features[k].getMethod().getScale();
-            int scaleType = -1;
-            double range = 0.0;
-            
-            switch (scale.getScaleType()) {
-                case NOMINAL:
-                    switch (scale.getDataType()) {
-                        case BOOLEAN:
-                            scaleType = BINARY_SCALE_TYPE; // assymetric binary
-                            break;
-                        default:
-                            scaleType = DISCRETE_SCALE_TYPE; // default nominal
-                    }
-                    break;
-                case INTERVAL:
-                case ORDINAL:
-                case RATIO:
-                    switch (scale.getDataType()) {
-                        case BIG_DECIMAL:
-                        case BIG_INTEGER:
-                        case DOUBLE:
-                        case FLOAT:
-                        case INTEGER:
-                        case LONG:
-                        case SHORT:
-                            scaleType = RANGED_SCALE_TYPE;
-                            range = scale.getMaximumValue().doubleValue() - scale.getMinimumValue().doubleValue();
-                            break;
-                        case BOOLEAN:
-                        case DATE:
-                        case STRING:
-                        case UNKNOWN:
-                        default:
-                            throw new IllegalArgumentException("Illegal data type " + scale.getDataType()
-                                                             + " for scale type " + scale.getScaleType());
-                    }
-                    break;
-                case NONE:
-                default:
-                    throw new IllegalArgumentException("Illegal scale type: " + scale.getScaleType());
-            }
-            
-            double distance = distance(scaleType, range, values[idX][k], values[idY][k], missingDataPolicy);
-            double weight = weight(scaleType, values[idX][k], values[idY][k]);
+            double distance = distance(scaleTypes[k], ranges[k],
+                                       rowX.getValue(k), rowY.getValue(k),
+                                       missingValuesPolicy);
+            double weight = weight(scaleTypes[k], rowX.getValue(k), rowY.getValue(k));
             distSum += distance * weight;
             weightSum += weight;
             
@@ -117,9 +116,66 @@ public class GowerDistance extends CachedDistanceMeasure {
         
     }
     
+    private FeatureMetadata getFeatureMetadata(FeatureData data){
+        // retrieve from cache (if present)
+        FeatureMetadata metadata = cache.get(data);
+        if(metadata == null){
+            // infer scale types and ranges
+            List<Feature> features = data.getFeatures();
+            int numFeatures = features.size();
+            int[] scaleTypes = new int[numFeatures];
+            double[] ranges = new double[numFeatures];
+            for(int k = 0; k < numFeatures; k++){
+                Scale scale = features.get(k).getMethod().getScale();
+                switch (scale.getScaleType()) {
+                    case NOMINAL:
+                        switch (scale.getDataType()) {
+                            case BOOLEAN:
+                                scaleTypes[k] = BINARY_SCALE_TYPE; // assymetric binary
+                                break;
+                            default:
+                                scaleTypes[k] = DISCRETE_SCALE_TYPE; // default nominal
+                        }
+                        break;
+                    case INTERVAL:
+                    case ORDINAL:
+                    case RATIO:
+                        switch (scale.getDataType()) {
+                            case BIG_DECIMAL:
+                            case BIG_INTEGER:
+                            case DOUBLE:
+                            case FLOAT:
+                            case INTEGER:
+                            case LONG:
+                            case SHORT:
+                                scaleTypes[k] = RANGED_SCALE_TYPE;
+                                ranges[k] = scale.getMaximumValue().doubleValue()
+                                          - scale.getMinimumValue().doubleValue();
+                                break;
+                            case BOOLEAN:
+                            case DATE:
+                            case STRING:
+                            case UNKNOWN:
+                            default:
+                                throw new IllegalArgumentException("Illegal data type " + scale.getDataType()
+                                                                 + " for scale type " + scale.getScaleType());
+                        }
+                        break;
+                    case NONE:
+                    default:
+                        throw new IllegalArgumentException("Illegal scale type: " + scale.getScaleType());
+                }
+            }
+            // combine in metadata and store in cache
+            metadata = new FeatureMetadata(numFeatures, scaleTypes, ranges);
+            cache.put(data, metadata);
+        }
+        return metadata;
+    }
+    
     private double distance(int scaleType, double range,
                             Object elementA, Object elementB,
-                            MissingValuesPolicy missingDataPolicy) {
+                            MissingValuesPolicy missingValuesPolicy) {
         if (elementA != null && elementB != null) {
             switch (scaleType) {
                 case BINARY_SCALE_TYPE:
@@ -144,7 +200,7 @@ public class GowerDistance extends CachedDistanceMeasure {
                     );
             }
         }
-        return missingValueContribution(missingDataPolicy, 1.0);
+        return missingValueContribution(missingValuesPolicy, 1.0);
     }
 
     private double weight(int scaleType, Object elementA, Object elementB) {
