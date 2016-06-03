4a7e8b5afe4828614577ff852880e0350dfc5237
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-06-03 16:53:23 +0200

    dedicated API for simple communication with R

---
+++ corehunter-base/src/main/java/org/corehunter/API.java
@@ -0,0 +1,108 @@
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
+package org.corehunter;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import org.corehunter.data.DistanceMatrixData;
+import org.corehunter.data.simple.SimpleDistanceMatrixData;
+import uno.informatics.data.SimpleEntity;
+import uno.informatics.data.io.FileType;
+import uno.informatics.data.pojo.SimpleEntityPojo;
+
+/**
+ * Simple API used by the R interface.
+ *
+ * @author Herman De Beukelaer, Guy Davenport
+ */
+public class API {
+
+    /************************/
+    /* Distance matrix data */
+    /************************/
+    
+    public static DistanceMatrixData readDistanceMatrixData(String file) throws IOException{
+        return SimpleDistanceMatrixData.readData(Paths.get(file), inferFileType(file));
+    }
+    
+    public static DistanceMatrixData createDistanceMatrixData(double[][] distances, String[] ids, String[] names){
+        // combine ids and names into headers
+        int n = distances.length;
+        if(ids == null){
+            throw new IllegalArgumentException("Ids are required.");
+        }
+        if(ids.length != n){
+            throw new IllegalArgumentException("Number of ids does not correspond to size of matrix.");
+        }
+        if(names == null){
+            // use ids as names if no names are given
+            names = ids;
+        }
+        if(names.length != n){
+            throw new IllegalArgumentException("Number of names does not correspond to size of matrix.");
+        }
+        SimpleEntity[] headers = new SimpleEntity[n];
+        for(int i = 0; i < n; i++){
+            headers[i] = new SimpleEntityPojo(ids[i], names[i]);
+        }
+        // create and return data
+        return new SimpleDistanceMatrixData(headers, distances);
+    }
+    
+    /**
+     * Get a copy of the full distance matrix.
+     * 
+     * @return full distance matrix (copy)
+     */
+    public static double[][] getDistanceMatrix(DistanceMatrixData distances){
+        int n = distances.getSize();
+        double[][] dist = new double[n][n];
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = distances.getDistance(i, j);
+            }
+        }
+        return dist;
+    }
+    
+    
+    /***************************/
+    /* Private utility methods */
+    /***************************/
+    
+    /**
+     * Infer file type from the extension of the file.
+     * Only supports {@link FileType#TXT} and {@link FileType#CSV} and returns <code>null</code> for other extensions.
+     * 
+     * @param file file path
+     * @return file type
+     */
+    private static FileType inferFileType(String file){
+        file = file.toLowerCase();
+        if(file.endsWith(".txt")){
+            return FileType.TXT;
+        }
+        if(file.endsWith(".csv")){
+            return FileType.CSV;
+        }
+        return null;
+    }
+    
+}
