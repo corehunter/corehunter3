9aeee1926b1bad7a18ca44fd14164fbdedae540e
Guy Davenport <daveneti@gmail.com>
2017-01-03 11:25:13 +1300

    added PhenotypeData

--- corehunter-base/src/main/java/org/corehunter/data/simple/SimplePhenotypeData.java
+++ corehunter-base/src/main/java/org/corehunter/data/simple/SimplePhenotypeData.java
@@ -1,3 +1,22 @@
+/*--------------------------------------------------------------*/
+/* Licensed to the Apache Software Foundation (ASF) under one   */
+/* or more contributor license agreements.  See the NOTICE file */
+/* distributed with this work for additional information        */
+/* regarding copyright ownership.  The ASF licenses this file   */
+/* to you under the Apache License, Version 2.0 (the            */
+/* "License"); you may not use this file except in compliance   */
+/* with the License.  You may obtain a copy of the License at   */
+/*                                                              */
+/*   http://www.apache.org/licenses/LICENSE-2.0                 */
+/*                                                              */
+/* Unless required by applicable law or agreed to in writing,   */
+/* software distributed under the License is distributed on an  */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       */
+/* KIND, either express or implied.  See the License for the    */
+/* specific language governing permissions and limitations      */
+/* under the License.                                           */
+/*--------------------------------------------------------------*/
+
 package org.corehunter.data.simple;
 
 import java.io.IOException;
@@ -10,6 +29,7 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import org.corehunter.data.GenotypeDataFormat;
+import org.corehunter.data.PhenotypeData;
 import org.jamesframework.core.subset.SubsetSolution;
 
 import uno.informatics.common.io.IOUtilities;
@@ -21,7 +41,10 @@ import uno.informatics.data.dataset.FeatureDataRow;
 import uno.informatics.data.feature.array.ArrayFeatureData;
 import uno.informatics.data.io.FileType;
 
-public class SimplePhenotypeData extends ArrayFeatureData {
+/**
+ * @author Guy Davenport, Herman De Beukelaer
+ */
+public class SimplePhenotypeData extends ArrayFeatureData implements PhenotypeData{
 
     private static final String ID_HEADER = "X";
     private static final String SELECTED_HEADER = "SELECTED";
